2017017e2d7e095a2fcfa69a098e3af24c5ebf74
javi <qualopec@gmail.com>
2012-11-20 16:48:11 +0100

    fixed port removing by browsers

--- control/lib/gridmanager.js
+++ control/lib/gridmanager.js
@@ -41,6 +41,14 @@ wax.gm = function() {
       tileTemplate = tileTemplate.
                         replace(/[\(\)\?\$\*\+\^]/g,'.')
 
+      // the browser removes the port in the case it matchs with
+      // the default port of the protocol
+      if(tileTemplate.indexOf('http') === 0 && tileTemplate.indexOf(':80') != -1) {
+        tileTemplate.replace(':80', '...')
+      } else if(tileTemplate.indexOf('https') === 0 && tileTemplate.indexOf(':443') != -1) {
+        tileTemplate.replace(':443', '....')
+      }
+
       // replace the first {x}{y}{z} by (\\d+)
       return tileTemplate
         .replace(/\{x\}/,'(\\d+)')
