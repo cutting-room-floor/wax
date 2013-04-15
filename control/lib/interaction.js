a13219f992d615c9882b734713674ecc81a18b46
xavijam <xavijam@gmail.com>
2013-04-15 15:32:44 +0200

    removed weird character

--- control/lib/interaction.js
+++ control/lib/interaction.js
@@ -148,7 +148,7 @@ wax.interaction = function() {
             // but also wax.u.eventoffset will have failed, since this touch
             // event doesn't have coordinates
             interaction.click(e, _d);
-        } else if (evt.type === "MSPointerMove" || evt.type === "MSPointerUp") {
+        } else if (evt.type === "MSPointerMove" || evt.type === "MSPointerUp") {
             interaction.click(evt, pos);
         } else if (Math.round(pos.y / tol) === Math.round(_d.y / tol) &&
             Math.round(pos.x / tol) === Math.round(_d.x / tol)) {
